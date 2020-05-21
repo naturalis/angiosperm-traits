2606de82c44f82b55b8a3ecdbf87e261618d3ea7
estherdr <57358103+estherdr@users.noreply.github.com>
2020-05-21 12:43:56 +0200

    inserted the code for phyloglmstep.

--- phyloglmstep.R
+++ phyloglmstep.R
@@ -1,111 +1,120 @@
-library(dplyr)
+################################################
+### Stepwise selection for phylolm using AIC
+################################################
 
-# optional arguments during the recursion:
-# - `included` a boolean vector of the current variables in the model
-# - `models` a list of models and their AICs
-phyloglmstep <- function(formula, phy, data, ...) {
+phyloglmstep <- function(formula, starting.formula = NULL, data=list(), phy, 
+                         method = c("logistic_MPLE","logistic_IG10"),
+                         direction = c("both", "backward", "forward"), trace = 2,
+                         btol = 10, log.alpha.bound = 4, start.beta=NULL, 
+                         start.alpha=NULL, boot = 0, full.matrix = TRUE,
+                         k=2, ...) 
+{
+  ## initialize  
+  method = match.arg(method)	
+  direction = match.arg(direction)  
+  fit.full = phyloglm(formula, data, phy, method, btol, log.alpha.bound, start.beta, start.alpha, boot, full.matrix, ...)
+  response = fit.full$formula[[2]] # name of the response
+  covariates = attr(terms(formula), "term.labels") # name of the covariates
   
-  # Create Formula as a string, returns it parsed
-  create.formula <- function(dependent, predictors, included) {
-    pi <- predictors[included]
-    ps <- paste(pi, collapse = ' + ')
-    m <- sprintf( '%s ~ 1 + %s', paste(dependent), ps )
-    return(as.formula(m))
+  p = length(covariates)
+  if (p==0) stop("Your full model only has intercept. Model selection is not needed.")
+  plm.full = rep(1,p)
+  
+  ## create a formula from current model
+  create.formula <- function(plm) {
+    on = which(plm==1)
+    str = paste(response," ~ 1",sep="")
+    if (length(on) > 0) 
+      for (i in 1:length(on)) str = paste(str," + ",covariates[on[i]],sep="")
+    return(str)
   }
   
-  # Maybe Negate: whatever arg is provided, is negated if n = T
-  mn <- function(arg,n) {
-    if (n) {
-      return(!arg)
-    } else {
-      return(arg)
-    }
+  ## fit phylolm
+  fit <- function(plm) {
+    return(phyloglm(create.formula(plm), data, phy, method, btol, log.alpha.bound, start.beta, start.alpha, boot, full.matrix, ...))
   }
   
-  # first n=T, then n=F
-  iterate.fit <- function(predictors,included,n,models) {
-    
-    # named vector of AICs in current iteration
-    aics <- vector(mode="numeric",length=length(predictors[mn(included,n)]))
-    names(aics) <- predictors[mn(included,n)]
-    
-    # iterate over predictors; if not yet included, add and fit it, record AIC
-    j <- 1
-    for ( i in 1:length(predictors) ) {
-      if ( mn(included[i],n) ) {
-        inc <- included
-        inc[i] <- n
-        mod <- create.formula(dependent,predictors,inc)
-        if ( is.null(models) || ! ( c(mod) %in% c(models) ) ) {
-          tryCatch({
-            result <- phyloglm(mod,data,phy,method="logistic_MPLE",btol=30)
-            aics[j] <- result$aic
-          }, error = function(error_condition) {
-            message(sprintf('phyloglm problem for %s',format(mod)))
-          }, finally={
-            
-          })
-        }
-        j <- j + 1
-      }
-    } 
-    aics <- aics[aics > 0]
-    return(sort(aics))
-  }
+  ## plm.current is a binary vector of length p
+  ## where 0 at i-th position means excluding i-th covariate
   
-  # coerce in case it's a string, then parse
-  formula <- as.formula(formula)
-  predictors <- attr(terms(formula), "term.labels")
-  dependent <- formula[[2]]
-  
-  # fetch or initialize optional arguments
-  params <- list(...)
-  included <- params$included
-  aics <- params$aics
-  models <- params$models
-  if ( is.null(included) ) {
-    included <- vector( mode = "logical", length = length(predictors) )
-  }
-  
-  # fit fw models, pick best result, add it to the included, log result
-  current.aics <- iterate.fit(predictors,included,TRUE,models)
-  best.predictor <- names(current.aics)[1]
-  best.aic <- current.aics[1]
-  included[ match( best.predictor, predictors ) ] <- T
-  best.model <- create.formula( dependent, predictors, included )
-  message( sprintf( 'FW: aic=%f (%s)\n', best.aic, format(best.model) ) )
-  
-  # fit sw models, pick best result, maybe remove from included, log result
-  if ( length(predictors[included]) > 1 ) {
-    current.aics <- iterate.fit(predictors,included,FALSE,models)
-    best.predictor <- names(current.aics)[1]
-    best.sw.aic <- current.aics[1]
-    if ( best.sw.aic < best.aic ) {
-      best.aic <- best.sw.aic
-      included[ match( best.predictor, predictors ) ] <- F
-      best.model <- create.formula( dependent, predictors, included )
-      message( sprintf( 'SW: aic=%f (%s)\n', best.aic, format(best.model) ) )
-    }
-  }
-  
-  # first iteration, store result and recurse
-  if ( is.null(aics) ) {
-    aics <- c(best.aic)
-    models <- c(best.model)
-    phyloglmstep(formula,phy,data,included=included,aics=aics,models=models)
-    
+  if (direction == "forward") {
+    plm.current = rep(0,p) # only intercept
+    fit.current = fit(plm.current)
   } else {
-    
-    # result is better, store and recurse
-    #    if ( best.aic < min(aics) ) {
-    aics <- c(aics,best.aic)
-    models <- c(models,best.model)
-    phyloglmstep(formula,phy,data,included=included,aics=aics,models=models)
-    
-    #    } else {
-    
-    # no further improvement, return df
-    #      return(aics)
-    #    }
+    plm.current = plm.full # all covariates
+    fit.current = fit.full
   }
-}
+  
+  if (!is.null(starting.formula)) {
+    fit.current = phylolm(starting.formula, data, phy, method, btol, log.alpha.bound, start.beta, start.alpha, boot, full.matrix, ...)
+    covariates.current = attr(terms(starting.formula), "term.labels")
+    plm.current = rep(0,p)
+    position = match(covariates.current,covariates)
+    if (any(is.na(position))) stop("The starting model is not a submodel of the full model.")
+    plm.current[position] = 1
+  }
+  
+  if (trace>0) {
+    cat("----------\n")
+    cat(paste("Starting model: ",create.formula(plm.current),"\n",sep=""))
+    cat(paste("Direction: ",direction,"\n",sep=""))
+    cat(paste("AIC(k=",k,"): ",AIC(fit.current,k),"\n",sep=""))
+    #     cat("----------\n")
+  }
+  
+  flag = 0 # flag of termination
+  count = 1
+  while (flag == 0) {
+    flag = 1
+    plm.best = plm.current
+    fit.best = fit.current
+    
+    ### to preserve hierarchy priciple
+    terms.add = add.scope(formula(create.formula(plm.current)), 
+                          formula(create.formula(plm.full)))
+    terms.drop = drop.scope(formula(create.formula(plm.current)))
+    
+    for (i in 1:p) {
+      
+      plm.propose = plm.current
+      do.update = FALSE
+      if ((plm.current[i]==1)&&(direction %in% c("both", "backward"))
+          &&(covariates[i] %in% terms.drop)) {
+        plm.propose[i] = 0  # remove i-th covariate
+        do.update = TRUE
+      }
+      if ((plm.current[i]==0)&&(direction %in% c("both", "forward"))
+          &&(covariates[i] %in% terms.add)) {
+        plm.propose[i] = 1 # add i-th covariate
+        do.update = TRUE
+      }
+      
+      ## check if proposed model is better
+      if (do.update){
+        fit.propose = fit(plm.propose)
+        if (trace>1) {
+          cat(paste0("\tProposed: ",create.formula(plm.propose),"\n"))
+          cat(paste0("\tAIC(k=",k,"): ",AIC(fit.propose,k),"\n"))
+        }
+        if (AIC(fit.propose,k) < AIC(fit.best,k)) {
+          plm.best = plm.propose
+          fit.best = fit.propose
+          flag = 0
+        }
+      }
+    }
+    
+    ## Set current model as the best model
+    plm.current = plm.best
+    fit.current = fit.best
+    
+    if (trace>0) {
+      cat("----------\nStep ",count,"\n",sep="")
+      cat(paste("Current model: ",create.formula(plm.current),"\n",sep=""))
+      cat(paste("AIC(k=",k,"): ",AIC(fit.current,k),"\n",sep=""))
+    }
+    count = count + 1
+  }
+  if (trace>0) cat("---END\n")
+  return(fit.current)
+}  
